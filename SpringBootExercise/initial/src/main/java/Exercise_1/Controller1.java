1c4e301bde91e8b76f6a3eb6b6ce3fd623aecffa
Lorenzo <cap.lorenzo@yahoo.it>
2023-06-18 16:40:01 +0200

    SpringBootExercise_1

---
+++ SpringBootExercise/initial/src/main/java/Exercise_1/Controller1.java
@@ -0,0 +1,20 @@
+package Exercise_1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller1 {
+    @GetMapping("/hello")
+    public String index2() {
+        return "Hello World!";
+    }
+
+    @GetMapping ("/greeting")
+    public ResponseEntity<String> index3() {
+        return ResponseEntity.status(200).body("Good Afternoon!");
+    }
+
+}
+
